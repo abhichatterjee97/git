20170db6e62264071180e591e600714d9fe815aa
Abhi Chatterjee <ac26141@gmail.com>
2023-01-29 02:11:26 +0530

    use of querySelector and querySelectorAlll

--- app.js
+++ app.js
@@ -14,3 +14,14 @@ for(var i=0;i<li.length;i++)
 {
     li[i].style.color="yellow"
 }
+var item2=document.querySelector(".list-group-item:nth-child(2)")
+item2.style.background="green"
+var item3=document.querySelector(".list-group-item:nth-child(3)")
+item3.style.display="none"
+var items2=document.querySelectorAll(".list-group-item")
+items2[1].style.color="green"
+var odd=document.querySelectorAll("li:nth-child(odd)")
+for(var i=0;i<odd.length;i++)
+{
+    items2[i].style.background="green"
+}
