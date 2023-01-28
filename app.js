1a5e41485d805af7902ff34b7af1b84f3f97e9d8
Abhi Chatterjee <ac26141@gmail.com>
2023-01-29 01:07:58 +0530

    3rd element in list green background   all the elements in  list have bold font

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ header.style.borderBottom="solid 2px black"
 var item=document.querySelector('h2')
 item.style.color="green"
 item.style.fontFamily="bold"
+var items=document.getElementsByClassName("list-group-item")
+items[2].style.background="green"
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.fontFamily="bold"
+}
