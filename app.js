9265153f8c2cbf59832434ccdc911654da247a5c
Abhi Chatterjee <ac26141@gmail.com>
2023-01-28 22:35:00 +0530

    title border black add item bold and green

---
+++ app.js
@@ -0,0 +1,5 @@
+var header=document.getElementById("main-header")
+header.style.borderBottom="solid 2px black"
+var item=document.querySelector('h2')
+item.style.color="green"
+item.style.fontFamily="bold"
